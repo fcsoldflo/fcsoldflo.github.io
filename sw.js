52d9e7cabde7f6f501b353e9988a8f421209d284
fcsoldflo <1079155637@qq.com>
2025-07-14 22:50:11 +0800

    Site updated: 2025-07-14 22:50:10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1752167055448";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1752504599062";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
